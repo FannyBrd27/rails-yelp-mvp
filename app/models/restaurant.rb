17ad6086dbfb1451bdaf976eb66f3500718d0b65
Fanny Brouard <bfbrouard@gmail.com>
2018-11-21 12:31:28 +0100

    Database filled  with 5 fake restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,5 +2,7 @@ class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :name, :address, :category, presence: true, confirmation: { case_sensitive: false }
   validates :name, :address, :category, presence: { message: 'cannot be empty' }
-  validates :category, inclusion: { in: %w(chinese italian japanese french belgian), message: '%{value} is not in the R.S.P.C.A list' }
+  validates :category, inclusion: { in: %w(chinese italian japanese french belgian),
+    message: '%{value} is not in the R.S.P.C.A list' },
+    confirmation: { case_sensitive: false }
 end
